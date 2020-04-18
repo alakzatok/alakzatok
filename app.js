33c68247f4f424a57399601ab04d36d0f19083a9
Géczi Kornél <eltal@freemail.hu>
2020-04-18 19:04:00 +0200

    Felesleges dolgok törlése az app.js-ből.

--- app.js
+++ app.js
@@ -2,8 +2,6 @@
 const leftDiv = document.getElementById("left-content");
 const centerDiv = document.getElementById("center-content");
 const rightDiv = document.getElementById("right-content");
-const sideMenu = document.getElementById("side-menu");
-const kiIratas = document.getElementById("kiiratas");
 
 function kiIrBal(szoveg) {
   const kiiratasBal = document.getElementById("left-content");
@@ -615,17 +613,6 @@ window.onclick = function(event) {
 
   if (!event.target.matches('.dropbtn4')) {
     var dropdowns = document.getElementsByClassName("dropdown-content4");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-
-  if (!event.target.matches('.btn-close')) {
-    var dropdowns = document.getElementsByClassName("side-nav");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
