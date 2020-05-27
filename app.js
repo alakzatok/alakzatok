c1ffe1373f0c1887103fba746419ad32f5e66d2f
Konlll <hungarokornel@gmail.com>
2020-05-27 20:59:11 +0200

    app.js és index.html felesleges kódok törlése.

--- app.js
+++ app.js
@@ -1026,52 +1026,6 @@ function sideNavDropdown4(){
   document.getElementById("side-dropdown4").classList.toggle("show");
 }
 
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn1')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content1");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-
-  if (!event.target.matches('.dropbtn2')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content2");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-
-  if (!event.target.matches('.dropbtn3')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content3");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-
-  if (!event.target.matches('.dropbtn4')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content4");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
-
 /* Side navbar */
 function openSlideMenu() {
   document.getElementById("side-menu").classList.toggle("show");
